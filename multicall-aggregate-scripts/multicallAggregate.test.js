90a2854fa677a53f3fa9bcaad0c06cc8fcc6dee5
aaron-bcw <aaron@bcw.group>
2023-02-02 16:52:58 +0800

    updated tests

--- multicall-aggregate-scripts/multicallAggregate.test.js
+++ multicall-aggregate-scripts/multicallAggregate.test.js
@@ -1,19 +1,16 @@
 const multiCallAggregate = require('./multicallAggregate');
 
-describe('MulticallAggregate Arkhia Prod', () => { 
-    if (process.env.ARKHIA_TESTNET_KEY) {
-        test('getMultiCallData: Call Prod Testnet', async () => {
-            const data = await multiCallAggregate.getMultiCallData(`https://hedera.testnet.arkhia.io/json-rpc/v1/${process.env.ARKHIA_TESTNET_KEY}`);
-            expect(data).toHaveProperty('data');
-        });
-    }
+describe('MulticallAggregate Arkhia Prod', () => {
+    expect(process.env.ARKHIA_KEY).toBeDefined();
+    test('getMultiCallData: Call Arkhia Testnet', async () => {
+        const data = await multiCallAggregate.getMultiCallData(`https://hedera.testnet.arkhia.io/json-rpc/v1/${process.env.ARKHIA_KEY}`);
+        expect(data).toHaveProperty('data');
+    });
 
-    if (process.env.ARKHIA_MAINNET_KEY) {
-        test('getMutliCallData: Call Prod Mainnet', async () => {
-            const data = await multiCallAggregate.getMultiCallData(`https://hedera.testnet.arkhia.io/json-rpc/v1/${process.env.ARKHIA_MAINNET_KEY}`);
-            expect(data).toHaveProperty('data');
-        });
-    }
+    test('getMutliCallData: Call Arkhia Mainnet', async () => {
+        const data = await multiCallAggregate.getMultiCallData(`https://hedera.mainnet.arkhia.io/json-rpc/v1/${process.env.ARKHIA_KEY}`);
+        expect(data).toHaveProperty('data');
+    });
 });
 
 describe('MulticallAggregate Hashio', () => {
