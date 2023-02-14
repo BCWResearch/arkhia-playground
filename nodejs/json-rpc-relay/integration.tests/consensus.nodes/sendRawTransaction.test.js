205c937eae81e17097b39fc2ba9e34deb11732d1
aaron-bcw <aaron@bcw.group>
2023-02-14 09:31:13 +0800

    feat(AR-766): clean up

--- nodejs/json-rpc-relay/integration.tests/consensus.nodes/sendRawTransaction.test.js
+++ nodejs/json-rpc-relay/integration.tests/consensus.nodes/sendRawTransaction.test.js
@@ -1,21 +1,14 @@
 console.clear();
 
-const { Client, Hbar, ContractId } = require('@hashgraph/sdk');
+const { ContractId } = require('@hashgraph/sdk');
 
 require('dotenv').config({path: '.env'});
-// const { Provider } = require('@hashgraph/sdk/lib/Provider');
-// const { ethers } = require("ethers");
 
 const { ethers } = require("hardhat");
 
-// import { Greeter } from '../typechain-types';
-
-const operatorId = process.env.OPERATOR_ACCOUNT_ID;
 const operatorPrivateKey = process.env.OPERATOR_PRIVATE_KEY;
 const communityHashioTestnet = process.env.COMMUNITY_TESTNET_URL;
 const arkhiaJsonRpcRelayTestnet  = `${process.env.ARKHIA_TESTNET_URL}/${process.env.ARKHIA_API_KEY}`;
-
-const client = Client.forTestnet().setOperator(operatorId, operatorPrivateKey).setDefaultMaxTransactionFee(new Hbar(10));
 
 describe('RPC', () => {
   const init_message = 'initial_msg';
@@ -26,16 +19,12 @@ describe('RPC', () => {
   let contractID;
 
   beforeAll(async () => { 
-    provider = new ethers.providers.JsonRpcProvider(communityHashioTestnet);
-    console.log('yo1')
-    wallet = new ethers.Wallet(process.env.OPERATOR_PRIVATE_KEY, provider);
-    console.log('yo2')
+    provider = new ethers.providers.JsonRpcProvider(arkhiaJsonRpcRelayTestnet);
+    wallet = new ethers.Wallet(operatorPrivateKey, provider);
     const Greeter = await ethers.getContractFactory('Greeter', wallet);
-    console.log('yo3')
-    greeter = await Greeter.deploy('initial_msg');
-    console.log('yo4')
+    greeter = await Greeter.deploy(init_message);
     const receipt = await greeter.deployTransaction.wait();
-    console.log('yo5')
+    console.log('receipt: ', receipt);
     contractAddress = receipt.contractAddress;
     contractID = ContractId.fromSolidityAddress(contractAddress);
     console.log(contractID, contractAddress);
@@ -55,12 +44,5 @@ describe('RPC', () => {
     const tx = await ethers.provider.send('eth_sendRawTransaction', [signedTx]);
     console.log('tx: ', tx);
     expect(tx).toBeDefined();
-    // wait for consensus
-    // TODO: might remove
-    // await new Promise((resolve) => setTimeout(resolve, 10000));
-
-    // const resMirrorNode = await axios.get(`contracts/results/${tx}`);
-
-    // console.log('resMirrorNode.data', resMirrorNode.data.call_result)
   });
 });
--- nodejs/json-rpc-relay/scripts/deploy.js
+++ nodejs/json-rpc-relay/scripts/deploy.js
@@ -1,25 +1,20 @@
 const { ethers } = require("hardhat");
-// import { ethers } from "hardhat";
 require('dotenv').config({path: '.env'});
 
 const arkhiaJsonRpcRelayTestnet  = `${process.env.ARKHIA_TESTNET_URL}/${process.env.ARKHIA_API_KEY}`;
 const communityHashioTestnet = process.env.COMMUNITY_TESTNET_URL;
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider(communityHashioTestnet);
+  const provider = new ethers.providers.JsonRpcProvider(arkhiaJsonRpcRelayTestnet);
   const wallet = new ethers.Wallet(process.env.OPERATOR_PRIVATE_KEY, provider);
-  console.log('yo1')
   const Greeter = await ethers.getContractFactory('Greeter', wallet);
-  console.log('yo2')
   const greeter = await Greeter.deploy('initial_msg');
-  console.log('yo3');
   const contractAddress = (await greeter.deployTransaction.wait()).contractAddress;
-  console.log('yo3');
 
   console.log(`Greeter deployed to ${contractAddress}`);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
+// To be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
   console.error(error);
