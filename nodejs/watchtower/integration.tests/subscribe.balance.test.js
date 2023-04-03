62977a6a86ac007a28b1323e9d7e1a87106b42f8
Syed Mehdi <syed@bcw.group>
2023-04-03 14:16:10 +0500

    feat(AR-752): cleanup and refactor

--- nodejs/watchtower/integration.tests/subscribe.balance.test.js
+++ nodejs/watchtower/integration.tests/subscribe.balance.test.js
@@ -5,11 +5,9 @@ const urlHandler = require('../../handlers/url.handler');
 const watchtowerHandler = require('../../handlers/watchtower.handler');
 const io = require('socket.io-client');
 let socket;
-let topicSubscriptionPayload;
+let balanceSubscriptionPayload;
 const accountTestnet = process.env.TESTNET_ACCOUNT_ID;
 
-let messagesAmount = 3; // make sure we get a few topics down
-let messageCount = 0;
 let timeout = 10000;
 
 const verifyBalanceSubscriptionResponse = (msg) => {
@@ -45,35 +43,23 @@ const verifyBalancePayload = (data, account) => {
     expect(data.body.cryptogetAccountBalance.accountID.accountNum).toEqual(accountArr[2]);
 }
 
-const verifyTopicSubscriptionMessageStream = (message) => {
-    expect(message.error).toBeDefined();
-    expect(message.error).toEqual(false);
-    expect(message.uid).toBeDefined();
-    expect(message.data).toBeDefined();
-    expect(message.data).toHaveProperty('consensusTimestamp');
-    expect(message.data).toHaveProperty('message');
-    expect(message.data).toHaveProperty('runningHash');
-    expect(message.data).toHaveProperty('sequenceNumber');
-    expect(message.data).toHaveProperty('runningHashVersion');
-}
-
 describe('Subscribe to CryptoGetBalance [Watchtower] on Testnet', () => {
 
     beforeAll((done) => {
         messageCount = 0;
         socket = io(urlHandler.getWatchtowerUrlTestnet());
-        topicSubscriptionPayload = watchtowerHandler.getBalancePayload(accountTestnet);
+        balanceSubscriptionPayload = watchtowerHandler.getBalancePayload(accountTestnet);
         socket.on('connect', function () {
             done();
         });
     });
 
     it('Should have correct subscription payload', () => {
-        verifyBalancePayload(topicSubscriptionPayload, accountTestnet);
+        verifyBalancePayload(balanceSubscriptionPayload, accountTestnet);
     });
 
     it('Should have correct balance payload in response', (done) => {
-        socket.emit(`subscribe`, topicSubscriptionPayload, (msg) => {
+        socket.emit(`subscribe`, balanceSubscriptionPayload, (msg) => {
             verifyBalanceSubscriptionResponse(msg)
             done();
         });
@@ -81,7 +67,7 @@ describe('Subscribe to CryptoGetBalance [Watchtower] on Testnet', () => {
 
     it('Should have balance response when subscribing to CryptoGetBalance', (done) => {
 
-        socket.emit(`subscribe`, topicSubscriptionPayload, (msg) => {
+        socket.emit(`subscribe`, balanceSubscriptionPayload, (msg) => {
             socket.on(msg.listeners.data, function (message) {
                 expect(message).toBeDefined();
                 expect(message.error).toBeDefined();
@@ -93,7 +79,7 @@ describe('Subscribe to CryptoGetBalance [Watchtower] on Testnet', () => {
                 expect(message.data.cryptogetAccountBalance).toHaveProperty('accountID');
                 expect(message.data.cryptogetAccountBalance).toHaveProperty('balance');
                 expect(message.data.cryptogetAccountBalance.header).toBeDefined();
-                
+
                 expect(message.data.cryptogetAccountBalance.accountID).toBeDefined();
                 expect(message.data.cryptogetAccountBalance.accountID).toHaveProperty('accountNum');
                 expect(message.data.cryptogetAccountBalance.accountID.accountNum).toBeDefined();
