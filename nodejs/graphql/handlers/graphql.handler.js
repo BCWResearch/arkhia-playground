1bb21804363c3205b376e988ea401614e5495a22
paul-okpor <paul@bcw.group>
2023-04-26 03:02:58 +0100

    updated the GraphQL integration test

--- nodejs/graphql/handlers/graphql.handler.js
+++ nodejs/graphql/handlers/graphql.handler.js
@@ -1,87 +1,40 @@
 require('dotenv').config({path: '.env'});
-const axios = require('axios');
-// Arkhia config
 
-const url = process.env.ARKHIA_MAINNET_GRAPHQL;
-const apiKey = process.env.ARKHIA_API_KEY;
-const data = {
-    query: `{
-    account(input: {
-      entityId: {
-        shard: 0,
-        realm: 0,
-        num: 98
-      }
-    }) {
-      alias
-      autoRenewAccount {
-        entityId {
-          shard
-          realm
-          num
-        }
-      }
-      autoRenewPeriod
-      balance
-      createdTimestamp
-      declineReward
-      deleted
-      entityId {
-        shard
-        realm
-        num
-      }
-      expirationTimestamp
-      id
-      key
-      maxAutomaticTokenAssociations
-      memo
-      nonce
-      obtainer {
-        entityId {
-          shard
-          realm
-          num
-        }
-      }
-      pendingReward
-      receiverSigRequired
-      stakedAccount {
-        entityId {
-          shard
-          realm
-          num
-        }
-      }
-      stakePeriodStart
-      timestamp {
-        from
-        to
-      }
-      type
+const Curl = require('node-libcurl').Curl;
+const urlHandler = require('../../handlers/url.handler');
+
+class GraphQlHandler {
+    static async fetchData() {
+        const curl = new Curl();
+        const url = urlHandler.getGraphQLMainnetUrl();
+        const apiKey = process.env.ARKHIA_API_KEY;
+        const postData = JSON.stringify({
+            query: '{ account(input: { entityId: { shard: 0, realm: 0, num: 98 } }) { alias autoRenewAccount { entityId { shard realm num } } autoRenewPeriod balance createdTimestamp declineReward deleted entityId { shard realm num } expirationTimestamp id key maxAutomaticTokenAssociations memo nonce obtainer { entityId { shard realm num } } pendingReward receiverSigRequired stakedAccount { entityId { shard realm num } } stakePeriodStart timestamp { from to } type } }',
+            variables: {},
+        });
+
+        return new Promise((resolve, reject) => {
+            curl.setOpt(Curl.option.URL, url);
+            curl.setOpt(Curl.option.HTTPHEADER, [
+                `x-api-key: ${apiKey}`,
+                'Content-Type: application/json',
+            ]);
+            curl.setOpt(Curl.option.POSTFIELDS, postData);
+
+            curl.perform();
+
+            curl.on('end', (statusCode, data, headers) => {
+                curl.close();
+                resolve(JSON.parse(data));
+            });
+
+            curl.on('error', error => {
+                console.error('Curl error:', error);
+                curl.close();
+                reject(error);
+            });
+        });
     }
-  }`,
-    variables: {}
-};
-
-
-class graphqlHandler {
-    getData = async () => {
-        try {
-            const response = await axios.post(url, data, {
-                headers: {
-                    'x-api-key': apiKey,
-                    'Content-Type': 'application/json'
-                }
-            })
-            return response
-        } catch (e) {
-            console.error(e)
-        }
-
-    }
-
 }
 
-module.exports = Object.freeze(new graphqlHandler());
-
+module.exports = GraphQlHandler;
--- nodejs/graphql/integration.test/graphql.test.js
+++ nodejs/graphql/integration.test/graphql.test.js
@@ -4,17 +4,41 @@ const {expect} = require('chai');
 const graphQlHandler = require('../handlers/graphql.handler');
 
 
-describe('Account response', () => {
+const assertAccount = async () => {
+    const response = await graphQlHandler.fetchData()
+    expect(response).to.have.property('data');
+    expect(response.data).to.have.property('account');
+    expect(response.data.account).to.have.property('balance');
+    expect(response.data.account).to.have.property('entityId');
+    expect(response.data.account).to.have.property('key');
+    expect(response.data.account).to.have.property('stakePeriodStart');
+    expect(response.data.account).to.have.property('timestamp');
+    expect(response.data.account).to.have.property('type');
+    expect(response.data.account.balance).to.be.a('number');
+    expect(response.data.account.entityId).to.be.an('object');
+    expect(response.data.account.key).to.be.an('object');
+    expect(response.data.account.stakePeriodStart).to.be.a('string');
+    expect(response.data.account.timestamp).to.be.an('object');
+    expect(response.data.account.type).to.be.a('string');
 
-    it('should have the correct account number', async () => {
-        const response = await graphQlHandler.getData()
-        expect(response.data.account.entityId.num).to.equal(98);
+}
+
+const assertAccountID = async () => {
+    const response = await graphQlHandler.fetchData()
+    expect(response.data.account).to.have.property('id').that.is.not.null;
+    expect(response.data.account).to.have.property('type').that.is.not.null;
+
+}
+describe('GraphQL API test', () => {
+
+    it('Should have a data object with an account property', async function () {
+        return assertAccount()
     });
 
-    it('should have a non-null balance', async () => {
-        const response = await graphQlHandler.getData()
-        expect(response.data.account.balance).to.not.be.null;
+    it('Should have an account with a non-null ID and type', async function () {
+        return assertAccountID()
     });
+
 })
 
 
