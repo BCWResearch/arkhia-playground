d008f7849298d1a1126cdc272a4e0eb732087ac5
paul-okpor <paul@bcw.group>
2023-06-13 02:23:39 +0100

    Added tests to validate API key

--- nodejs/rest-api/handlers/rest.api.handler.js
+++ nodejs/rest-api/handlers/rest.api.handler.js
@@ -41,7 +41,127 @@ class RestApiHandler {
 
     // Rest of the methods with similar modifications
 
-    // ...
+    getTokens = async (isMainnet) => {
+        try {
+            const tokensUrl = `${urlHandler.getApiUrl(isMainnet)}/tokens`;
+            const response = await axios.get(tokensUrl, body);
+            return response;
+        } catch(e) {
+            console.error(e.response.data);
+        }
+    }
+
+    getTokenById = async (tokenId, isMainnet) => {
+        try {
+            const tokenUrl = `${urlHandler.getApiUrl(isMainnet)}/tokens/${tokenId}`;
+            const response = await axios.get(tokenUrl, body);
+            return response;
+        } catch(e) {
+            console.error(e.response.data);
+        }
+    }
+
+    getContracts = async (isMainnet) => {
+        try {
+            const contractUrl = `${urlHandler.getApiUrl(isMainnet)}/contracts`;
+            const response = await axios.get(contractUrl, body);
+            return response;
+        } catch(e) {
+            console.error(e.response.data);
+        }
+    }
+
+    getContractById = async (contract_id, isMainnet) => {
+        try {
+            const contractUrl = `${urlHandler.getApiUrl(isMainnet)}/contracts/${contract_id}`;
+            const response = await axios.get(contractUrl, body);
+            return response;
+        } catch(e) {
+            console.error(e.response.data);
+        }
+    }
+
+    getContractResultsById = async (contract_id, isMainnet) => {
+        try {
+            const contractUrl = `${urlHandler.getApiUrl(isMainnet)}/contracts/${contract_id}/results`;
+            const response = await axios.get(contractUrl, body);
+            return response;
+        } catch(e) {
+            console.error(e.response.data);
+        }
+    }
+
+    getContractResultLogsById = async (contract_id, isMainnet) => {
+        try {
+            const contractUrl = `${urlHandler.getApiUrl(isMainnet)}/contracts/${contract_id}/results/logs`;
+            const response = await axios.get(contractUrl, body);
+            return response;
+        } catch(e) {
+            console.error(e.response.data);
+        }
+    }
+
+    getContractEvmCall = async (payload, isMainnet) => {
+        try {
+            // works with url : `https://testnet.mirrornode.hedera.com/api/v1/contracts/call`
+            const contractUrl = `${urlHandler.getApiUrl(isMainnet)}/contracts/call`;
+            const response = await axios.post(contractUrl, payload, body);
+            return response;
+        } catch(e) {
+            console.log(`Error`);
+            console.error(e.response.data);
+            console.log(e.response.data._status);
+        }
+    }
+
+    getTransactions = async (isMainnet) => {
+        try {
+            const transactionsUrl = `${urlHandler.getApiUrl(isMainnet)}/transactions`;
+            const response = await axios.get(transactionsUrl, body);
+            return response;
+        } catch(e) {
+            console.error(e.response.data);
+        }
+    }
+
+    getTransactionByAccountId = async (accountId, isMainnet) => {
+        try {
+            const transactionsUrl = `${urlHandler.getApiUrl(isMainnet)}/transactions?account.id=${accountId}`;
+            const response = await axios.get(transactionsUrl, body);
+            return response;
+        } catch(e) {
+            console.error(e.response.data);
+        }
+    }
+
+    getNetworkNodes = async(isMainnet) => {
+        try {
+            const networkNodesUrl = `${urlHandler.getApiUrl(isMainnet)}/network/nodes`;
+            const response = await axios.get(networkNodesUrl, body);
+            return response;
+        } catch(e) {
+            console.error(e.response.data);
+        }
+    }
+    getBlocks = async (isMainnet) => {
+        try {
+            const blocksUrl = `${urlHandler.getApiUrl(isMainnet)}/blocks`;
+            const response = await axios.get(blocksUrl, body);
+            return response;
+        } catch(e) {
+            console.error(e.response.data);
+        }
+    }
+
+    getSchedules = async (isMainnet) => {
+        try {
+            const schedulesUrl = `${urlHandler.getApiUrl(isMainnet)}/schedules`;
+            const response = await axios.get(schedulesUrl, body);
+            return response;
+        } catch(e) {
+            console.error(e.response.data);
+        }
+    }
 
     getBalances = async (isMainnet) => {
         try {
