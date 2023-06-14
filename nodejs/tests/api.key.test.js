38cf900c2e66b0440ea1a57a32177258b8c0680d
paul-okpor <paul@bcw.group>
2023-06-14 16:12:38 +0100

    Add more tests to validate API key

--- nodejs/tests/api.key.test.js
+++ nodejs/tests/api.key.test.js
@@ -1,33 +1,40 @@
-require('dotenv').config();
+require("dotenv").config();
 console.clear();
 
-const axios = require('axios');
-const urlHandler = require('../handlers/url.handler');
-
+const axios = require("axios");
+const urlHandler = require("../handlers/url.handler");
 
 // Arkhia config
 const apiKey = process.env.ARKHIA_API_KEY;
-const headers = { 'x-api-key': apiKey };
+const headers = { "x-api-key": apiKey };
 
-describe('Test to validate API key', () => {
-    test('Should load correct API Key', () => {
-        expect(apiKey).toBeDefined();
-        expect(apiKey.length).toEqual(32);
-    });
+describe("Test to validate API key", () => {
+  test("The API key is available and has correct length", () => {
+    expect(apiKey).toBeDefined();
+    expect(apiKey.length).toEqual(32);
+  });
 
-    test('this API Key is not valid', async () => {
-        try {
-            const balancesUrl = `${urlHandler.getApiUrl()}/balances`;
-            const response = await axios.get(balancesUrl, { headers });
-        } catch (error) {
-            expect((error.response.data)).toHaveProperty('status', false);
-            expect(error.response.data.response).toContain('Unauthorized');
-            console.error(error.response.data.response)
-        }
+  test("The API Key is authorized", async () => {
+    try {
+      const balancesUrl = `${urlHandler.getApiUrl()}/balances`;
+      const response = await axios.get(balancesUrl, { headers });
+    } catch (error) {
+      expect(error.response.data).toHaveProperty("status", false);
+      expect(error.response.data.response).toContain("Unauthorized");
+      console.error(error.response.data.response);
+    }
+  });
 
-
-    });
-
-
-
+  test("The API Key is valid for its tier", async () => {
+    try {
+      const balancesUrl = `${urlHandler.getApiUrl()}/balances`;
+      const response = await axios.get(balancesUrl, { headers });
+    } catch (error) {
+      expect(error.response.data).toHaveProperty("status", false);
+      expect(error.response.data.response).toContain(
+        "You have upgraded your account to a paid tier. Please use the upgraded URL for an enhanced experience"
+      );
+      console.error(error.response.data.response);
+    }
+  });
 });
