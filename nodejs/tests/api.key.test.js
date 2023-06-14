798d8c85bac534970446872801e0aab792552ca4
paul-okpor <paul@bcw.group>
2023-06-14 02:54:39 +0100

    modified code to throw error for invalid API key

--- nodejs/tests/api.key.test.js
+++ nodejs/tests/api.key.test.js
@@ -10,15 +10,24 @@ const apiKey = process.env.ARKHIA_API_KEY;
 const headers = { 'x-api-key': apiKey };
 
 describe('Test to validate API key', () => {
-    test('Should load API Key', () => {
+    test('Should load correct API Key', () => {
         expect(apiKey).toBeDefined();
         expect(apiKey.length).toEqual(32);
     });
 
-    test('Should validate API Key', async () => {
-        const balancesUrl = `${urlHandler.getApiUrl()}/balances`;
-        const response = await axios.get(balancesUrl, { headers });
-        expect(response).toHaveProperty('status', 200);
-        expect(response.data.status).not.toBe(false);
+    test('this API Key is not valid', async () => {
+        try {
+            const balancesUrl = `${urlHandler.getApiUrl()}/balances`;
+            const response = await axios.get(balancesUrl, { headers });
+        } catch (error) {
+            expect((error.response.data)).toHaveProperty('status', false);
+            expect(error.response.data.response).toContain('Unauthorized');
+            console.error(error.response.data.response)
+        }
+
+
     });
+
+
+
 });
