1245ab819288641edceb1b4ab3b7719fd20637bc
Syed Mehdi <117643207+syed-bcw@users.noreply.github.com>
2023-10-03 11:55:04 +0500

    feat(AR-1265): Mainnet Verify ETH_CALL_DEFAULT_TO_CONSENSUS_NODE = FALSE (#35)

--- nodejs/json-rpc-relay/integration.tests/contracts/mainnet/call.contract.mainnet.test.js
+++ nodejs/json-rpc-relay/integration.tests/contracts/mainnet/call.contract.mainnet.test.js
@@ -1,15 +1,21 @@
 console.clear();
 
-require('dotenv').config({path: '.env'});
+require('dotenv').config({ path: '.env' });
 const Web3 = require('web3');
 const { ethers } = require("ethers");
 const urlHandler = require('../../../../handlers/url.handler');
-const privateECDSAAccount = process.env.MAINNET_OPERATOR_PRIVATE_KEY;
+const ERC20 = require('../artifact/erc20abi.json');
+const privateECDSAAccount = process.env.MAINNET_OPERATOR_PRIVATE_EVM_KEY;
 const contractAbi = `[{"inputs":[{"internalType":"string","name":"_creatorName","type":"string"},{"internalType":"string","name":"_tokenSymbol","type":"string"},{"internalType":"string","name":"_tokenName","type":"string"},{"internalType":"uint256","name":"_tokenSupply","type":"uint256"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":false,"internalType":"uint256","name":"timestamp","type":"uint256"},{"indexed":false,"internalType":"string","name":"name","type":"string"},{"indexed":false,"internalType":"string","name":"message","type":"string"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"FairTradeEvent","type":"event"},{"stateMutability":"payable","type":"fallback"},{"inputs":[],"name":"getContractBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getContractMetadata","outputs":[{"components":[{"internalType":"string","name":"creatorName","type":"string"},{"internalType":"string","name":"tokenSymbol","type":"string"},{"internalType":"string","name":"tokenName","type":"string"},{"internalType":"uint256","name":"tokenSupply","type":"uint256"},{"internalType":"address","name":"tokenAddress","type":"address"}],"internalType":"struct FairTradeCoffee.FairTradeMetadata","name":"","type":"tuple"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getFairTradeBuyerNumbers","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getFairTradeBuyers","outputs":[{"components":[{"internalType":"address","name":"from","type":"address"},{"internalType":"uint256","name":"timestamp","type":"uint256"},{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"message","type":"string"},{"internalType":"uint256","name":"amount","type":"uint256"}],"internalType":"struct FairTradeCoffee.FairTradeBuyer[]","name":"","type":"tuple[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getTokenRemainingBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"string","name":"_name","type":"string"},{"internalType":"string","name":"_message","type":"string"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"makeDonationHbars","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"mintFungibleToken","outputs":[{"internalType":"address","name":"createdTokenAddress","type":"address"}],"stateMutability":"payable","type":"function"},{"inputs":[],"name":"withdrawDonations","outputs":[],"stateMutability":"nonpayable","type":"function"},{"stateMutability":"payable","type":"receive"}]`;
 
 const mainnetContractConfig = {
     id: `0.0.2935861`,
     sol_id: `0x00000000000000000000000000000000002ccc35`
+}
+
+const mainnetUSDCContractConfig = {
+    id: `0.0.456858`,
+    sol_id: `0x000000000000000000000000000000000006f89a`
 }
 
 describe('[Web3] Call Contract Method', () => {
@@ -48,6 +54,27 @@ describe('[Ethers] Call Contract Method', () => {
         expect(Number(contractBalance)).toBeGreaterThanOrEqual(0);
     });
 
+    test('Mainnet | Verify ETH_CALL_DEFAULT_TO_CONSENSUS_NODE = FALSE', async () => {
+        // Arrange
+        const provider = new ethers.providers.JsonRpcProvider(urlHandler.getJsonRpcMainnet());
+        const signer = new ethers.Wallet(privateECDSAAccount, provider);
+        const usdcContract = new ethers.Contract(mainnetUSDCContractConfig.sol_id, ERC20.abi, signer)
+
+        const responseTime = [];
+
+        // Act
+        for (let i = 0; i < 5; i++) {
+            const startTime = new Date().getTime();
+            await usdcContract.name();
+            const endTime = new Date().getTime();
+            responseTime.push(endTime - startTime);
+        }
+        // Assert
+        // sort response time from fastest to slowest and assert if median is under milliseconds
+        const sortedResponseTime = responseTime.sort((a, b) => a - b);
+        const medianResponseTime = sortedResponseTime[2];
+        expect(medianResponseTime).toBeLessThan(1000);
+    });
 
 });
 
