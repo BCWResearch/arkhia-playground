ff4d99d6e9965b1010d4007096f48eaa9cb2f70d
Paul Okpor <paul@bcw.group>
2024-03-06 08:36:57 +0100

    feat(AR-1513): removed api key hard coding

--- nodejs/graphql/handlers/subgraph.handler.js
+++ nodejs/graphql/handlers/subgraph.handler.js
@@ -2,7 +2,7 @@ require('dotenv').config({path: '.env'});
 const urlHandler = require('../../handlers/url.handler');
 const axios = require('axios');
 
-const query = JSON.stringify({query: '{ transfers(first: 5) { id sender recipient amount }'});
+ const apiKey = process.env.ARKHIA_API_KEY
 
 
 
@@ -10,7 +10,7 @@ class SubgraphHandler {
     getTransfers = async (isMainnet)=>{
 
         try {
-            const response = await axios.post(`${urlHandler.getSubgraphURL(isMainnet)}/aba76682a6nb4K6bDDN6aa71N1c4NN8a/erc20-transfers-quickstart/`, {
+            const response = await axios.post(`${urlHandler.getSubgraphURL(isMainnet)}/${apiKey}/erc20-transfers-quickstart/`, {
                 query: 'query { transfers(first: 5) { id sender recipient amount } }',
                 variables: {}
             }, {
